9a93d4a2015aa5c3e5abb1a1025cf240e76c5981
Nikita <tumanstiv@mail.ru>
2022-09-05 20:21:05 +0300

    Second HomeWork

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Ведите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit = num / 10;
+int secondDigit = digit % 10;
+if (num > 99 && num < 1000) Console.Write($"{num} -> {secondDigit}");
+else Console.Write("Ошибка, введите трёхзначное число");
---
+++ Trask13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры не
+// 32679 -> 6
+
+
+Console.Clear();
+Console.Write("Ведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = num;
+if (num > 99)
+{
+    while (count > 999)
+    {
+        count = count / 10;
+    }
+    int thirdDigit = count % 10;
+    Console.Write($"{num} -> {thirdDigit}");
+}
+else Console.Write("Третьей цифры нет ");
