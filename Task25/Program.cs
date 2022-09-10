603e3e957d22056e210ac0932185ed9c02b67620
Nikita <tumanstiv@mail.ru>
2022-09-10 14:42:48 +0300

    Fouth HomeWork

---
+++ Task25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.Clear();
+Console.Write("Введите число   : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральную степень : ");
+int b = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+
+object Pow(int a1, int b1, int num1)
+{
+    if (b1 > 0)
+    {
+        for (int i = 0; i < b1; i++)
+        {
+            num1 = num1 * a1;
+        }
+        return $"{a1} в степени {b1} = {num1}";
+    }
+    return "Ошибка, введите натуральную степень";
+};
+
+object power = Pow(a, b, num);
+Console.Write(power);
+
+
+
---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Console.Clear();
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = default;
+
+int Summa(int num1, int sum1)
+{
+    {
+        while (num1 > 0)
+        {
+            sum1 = sum1 + num1 % 10;
+            num1 = num1 / 10;
+        }
+    }
+    return sum1;
+}
+
+sum = Summa(num, sum);
+Console.WriteLine(sum);
+
