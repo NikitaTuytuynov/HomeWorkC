f761007c47c9275053f3831750daf988fa049fb4
Nikita <tumanstiv@mail.ru>
2022-09-20 19:08:49 +0300

    Sixth HomeWork

---
+++ Task41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// // 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите число элементов массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] ArrayCreate(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите число № {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ArrayPrint(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int ArrayCount(int[] array)
+{
+    int cou = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) cou++;
+    }
+    return cou;
+
+}
+
+int[] arr = ArrayCreate(size);
+ArrayPrint(arr);
+int count = ArrayCount(arr);
+Console.Write($"Положительных чисел :{count} ");
---
+++ Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+Console.Write("Ведите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double FindX(double b10, double k10, double b20, double k20)
+{
+    double x1 = Math.Round((-b20 + b10) / (-k10 + k20), 2);
+    return x1;
+}
+double FindY(double k10, double b10, double x1)
+{
+    double y1 = Math.Round(((k10 * x1) + b10), 2);
+    return y1;
+}
+
+double x = FindX(b1, k1, b2, k2);
+double y = FindY(k1,b1,x);
+Console.Write($"Точка перечечения Х:{x}, Точка перечечения Y:{y} ");
